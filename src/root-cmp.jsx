95583fb57f30b466c5094f9f484f77408ad5f065
Noga Mor-Havilio <nogamorh@gmail.com>
2022-10-06 12:18:05 +0300

    Basic app structure

---
+++ src/root-cmp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Routes, Route } from 'react-router'
+
+import './assets/styles/main.scss'
+import { Home } from './pages/home'
+
+export class RootCmp extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route key='/' element={<Home />} path='/'></Route>
+        </Routes>
+      </div>
+    )
+  }
+}
